011d79b734007df3300f2d9d08b9f41910c0d4f5
Adepitan <akoredeadetunji93@gmail.com>
2025-09-09 10:30:28 +0100

    Fixing filters

--- crm/models.py
+++ crm/models.py
@@ -15,6 +15,7 @@ class Customer(models.Model):
             )
         ]
     )
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.name} ({self.email})"
@@ -35,6 +36,7 @@ class Order(models.Model):
     products = models.ManyToManyField(Product, related_name="orders")
     order_date = models.DateTimeField(auto_now_add=True)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    order_date = models.DateTimeField(auto_now_add=True)
 
     def calculate_total(self):
         total = sum([p.price for p in self.products.all()])
